b2288a388963063aaa1e89552256e7c8a2ba3d8a
tarcies <54951819+tarcies@users.noreply.github.com>
2026-09-02 20:57:35 +1000

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false;
+        const map = {};
+
+        for (const char of s) {
+            // map.set(char, (map.get(char) || 0) + 1);
+            map[char] = (map[char] || 0) + 1;
+        }
+
+        for (const char of t) {
+            // map.set(char, (map.get(char) || 0) - 1);
+            map[char] = (map[char] || 0) - 1;
+
+            if (map[char] == 0) delete map[char];
+        }
+
+        return Object.keys(map).length == 0 ? true : false;
+    }
+
+}
