df883cfc123e71cec8459d34b1e19826d2e10cd9
tarcies <54951819+tarcies@users.noreply.github.com>
2026-09-02 20:53:09 +1000

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false;
+        const map = new Map();
+
+        for (const char of s) {
+            map.set(char, (map.get(char) || 0) + 1);
+        }
+
+        for (const char of t) {
+            map.set(char, (map.get(char) || 0) - 1);
+            if (map.get(char) == 0) {
+                map.delete(char);
+            }
+        }
+
+        return map.size == 0 ? true : false;
+    }
+
+}
